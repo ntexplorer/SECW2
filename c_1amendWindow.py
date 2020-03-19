17e963da42c5cfdf37fad548f9ab71d75c36dd12
clarkey02 <61282465+clarkey02@users.noreply.github.com>
2020-03-19 16:52:00 +0000

    version4

--- c_1amendWindow.py
+++ c_1amendWindow.py
@@ -23,18 +23,19 @@ class amendQuestion(Frame):
         self.newID = 0
 
         if (self.Ans.radioValType == 1):
-            question = questionMCClass()
+            self.question = questionMCClass()
+            self.outgoingQuestion = questionMCClass()
             with conn:
                 cur = conn.execute("SELECT * FROM MC_QUESTION WHERE PID = {i}".format(i=qID))
                 for i in (cur.fetchall()):
-                    question.qID = i[0]
-                    question.question = (str(i[1]))
-                    question.category = (str(i[2]))
-                    question.difficulty = (str(i[3]))
-                    question.correct = (str(i[4]))
-                    question.wrong1 = (str(i[5]))
-                    question.wrong2 = (str(i[6]))
-                    question.wrong3 = (str(i[7]))
+                    self.question.qID = i[0]
+                    self.question.question = (str(i[1]))
+                    self.question.category = (str(i[2]))
+                    self.question.difficulty = (str(i[3]))
+                    self.question.correct = (str(i[4]))
+                    self.question.wrong1 = (str(i[5]))
+                    self.question.wrong2 = (str(i[6]))
+                    self.question.wrong3 = (str(i[7]))
 
                 cur = conn.execute("SELECT PID FROM MC_QUESTION")
                 for i in (cur.fetchall()):
@@ -44,15 +45,16 @@ class amendQuestion(Frame):
 
 
         elif (self.Ans.radioValType == 2):
-            question = questionTFClass()
+            self.question = questionTFClass()
+            self.outgoingQuestion = questionTFClass()
             with conn:
                 cur = conn.execute("SELECT * FROM TF_QUESTION WHERE PID = {i}".format(i=qID))
                 for i in (cur.fetchall()):
-                    question.qID = i[0]
-                    question.question = (str(i[1]))
-                    question.category = (str(i[2]))
-                    question.difficulty = (str(i[3]))
-                    question.answer = (str(i[4]))
+                    self.question.qID = i[0]
+                    self.question.question = (str(i[1]))
+                    self.question.category = (str(i[2]))
+                    self.question.difficulty = (str(i[3]))
+                    self.question.answer = (str(i[4]))
 
                 cur = conn.execute("SELECT PID FROM TF_QUESTION")
                 for i in (cur.fetchall()):
@@ -63,22 +65,34 @@ class amendQuestion(Frame):
         conn.commit()
 
         self.grid()
-        self.create_buttons()
 
         if (self.Ans.radioValType == 1):
-            self.create_labelsMC(question)
-            self.create_inputsMC(question)
+            self.create_labelsMC()
+            self.create_inputsMC()
         if (self.Ans.radioValType == 2):
-            self.create_labelsTF(question)
-            self.create_inputsTF(question)
+            self.create_labelsTF()
+            self.create_inputsTF()
+
+        self.create_buttons()
 
     def create_buttons(self):
 
         buttonAmend = Button(self, text='Amend Question', font=('Helvetica', 15), justify="center")
-        # buttonAmend['command']=self.submitAmend
         buttonAmend.grid(row=13, column=2, padx=10, pady=10)
 
-    def create_labelsMC(self, question):
+        if (self.question.type == "Multiple Choice"):
+            buttonAmend['command']=self.addOutgoingMC
+
+        elif (self.question.type == "True/ False"):
+            buttonAmend['command']=self.createOutgoingTF
+
+    def create_labelsMC(self):
+
+        lblInfo = Label (self, text="A list of details associated with the question ID specified previously are" +
+                        " summarised below.\n" "You may edit (or leave untouced) some of these details and amend/create a revised verison.\n" +
+                        " This can be added to the database by clicking the button below.\n" + "A new question ID will be generated, however "
+                        + "the former ID will not be deleted.", font=("Helvetica", 10))
+        lblInfo.grid(row=1, column=2, padx=10, pady=10)
 
         lblFormerID = Label(self, text="Former ID:", font=("Helvetica", 12, "bold"))
         lblFormerID.grid(row=2, column=1, padx=10, pady=10)
@@ -110,7 +124,7 @@ class amendQuestion(Frame):
         lblWrongAnswer3 = Label(self, text="Wrong Answer 3:", font=("Helvetica", 12, "bold"))
         lblWrongAnswer3.grid(row=11, column=1, padx=10, pady=10)
 
-    def create_labelsTF(self, question):
+    def create_labelsTF(self):
 
         lblFormerID = Label(self, text="Former ID:", font=("Helvetica", 12, "bold"))
         lblFormerID.grid(row=2, column=1, padx=10, pady=10)
@@ -133,66 +147,115 @@ class amendQuestion(Frame):
         lblTFAnswer = Label(self, text="Answer:", font=("Helvetica", 12, "bold"))
         lblTFAnswer.grid(row=8, column=1, padx=10, pady=10)
 
-    def create_inputsMC(self, question):
+    def create_inputsMC(self):
 
-        lblFQID = Label(self, text=str(question.qID), font=("Helvetica", 12))
+        lblFQID = Label(self, text=str(self.question.qID), font=("Helvetica", 12))
         lblFQID.grid(row=2, column=2, padx=10, pady=10)
 
         lblNQID = Label(self, text=self.newID, font=("Helvetica", 12))
         lblNQID.grid(row=3, column=2, padx=10, pady=10)
 
-        lblCI = Label(self, text=question.category, font=("Helvetica", 12))
+        lblCI = Label(self, text=self.question.category, font=("Helvetica", 12))
         lblCI.grid(row=4, column=2, padx=10, pady=10)
 
-        lblTI = Label(self, text=question.type, font=("Helvetica", 12))
+        lblTI = Label(self, text=self.question.type, font=("Helvetica", 12))
         lblTI.grid(row=5, column=2, padx=10, pady=10)
 
         self.entDifficulty = Entry(self, width=100)
         self.entDifficulty.grid(row=6, column=2)
-        self.entDifficulty.insert(0, question.difficulty)
+        self.entDifficulty.insert(0, self.question.difficulty)
 
         self.entQuestion = Entry(self, width=100)
         self.entQuestion.grid(row=7, column=2)
-        self.entQuestion.insert(0, question.question)
+        self.entQuestion.insert(0, self.question.question)
 
         self.entCorrect = Entry(self, width=100)
         self.entCorrect.grid(row=8, column=2)
-        self.entCorrect.insert(0, question.correct)
+        self.entCorrect.insert(0, self.question.correct)
 
         self.entWrong1 = Entry(self, width=100)
         self.entWrong1.grid(row=9, column=2)
-        self.entWrong1.insert(0, question.wrong1)
+        self.entWrong1.insert(0, self.question.wrong1)
 
         self.entWrong2 = Entry(self, width=100)
         self.entWrong2.grid(row=10, column=2)
-        self.entWrong2.insert(0, question.wrong2)
+        self.entWrong2.insert(0, self.question.wrong2)
 
         self.entWrong3 = Entry(self, width=100)
         self.entWrong3.grid(row=11, column=2)
-        self.entWrong3.insert(0, question.wrong3)
+        self.entWrong3.insert(0, self.question.wrong3)
 
-    def create_inputsTF(self, question):
+    def create_inputsTF(self):
 
-        lblFQID = Label(self, text=str(question.qID), font=("Helvetica", 12))
+        lblFQID = Label(self, text=str(self.question.qID), font=("Helvetica", 12))
         lblFQID.grid(row=2, column=2, padx=10, pady=10)
 
         lblNQID = Label(self, text=self.newID, font=("Helvetica", 12))
         lblNQID.grid(row=3, column=2, padx=10, pady=10)
 
-        lblCI = Label(self, text=question.category, font=("Helvetica", 12))
+        lblCI = Label(self, text=self.question.category, font=("Helvetica", 12))
         lblCI.grid(row=4, column=2, padx=10, pady=10)
 
-        lblTI = Label(self, text=question.type, font=("Helvetica", 12))
+        lblTI = Label(self, text=self.question.type, font=("Helvetica", 12))
         lblTI.grid(row=5, column=2, padx=10, pady=10)
 
         self.entDifficulty = Entry(self, width=100)
         self.entDifficulty.grid(row=6, column=2)
-        self.entDifficulty.insert(0, question.difficulty)
+        self.entDifficulty.insert(0, self.question.difficulty)
 
         self.entQuestion = Entry(self, width=100)
         self.entQuestion.grid(row=7, column=2)
-        self.entQuestion.insert(0, question.question)
+        self.entQuestion.insert(0, self.question.question)
+
+        if (self.question.answer == "1"):
+            self.question.answer = "True"
+
+        if (self.question.answer == "0"):
+            self.question.answer ="False"
 
         self.entCorrect = Entry(self, width=100)
         self.entCorrect.grid(row=8, column=2)
-        self.entCorrect.insert(0, question.answer)
+        self.entCorrect.insert(0, self.question.answer)
+
+    def createOutgoingMC(self):
+
+        self.outgoingQuestion.qID = self.newID
+        self.outgoingQuestion.category = self.question.category
+        self.outgoingQuestion.difficulty = self.entDifficulty.get()
+        self.outgoingQuestion.question = self.entQuestion.get()
+        self.outgoingQuestion.correct = self.entCorrect.get()
+        self.outgoingQuestion.wrong1 = self.entWrong1.get()
+        self.outgoingQuestion.wrong2 = self.entWrong2.get()
+        self.outgoingQuestion.wrong3 = self.entWrong3.get()
+
+    def createOutgoingTF(self):
+
+        self.outgoingQuestion.qID = self.newID
+        self.outgoingQuestion.category = self.question.category
+        self.outgoingQuestion.difficulty = self.entDifficulty.get()
+        self.outgoingQuestion.question = self.entQuestion.get()
+        self.outgoingQuestion.answer = self.entCorrect.get()
+
+    def addOutgoingMC(self):
+
+        createOutgoingMC()
+
+        conn = sqlite3.connect('system.db')
+        c = conn.cursor ()
+
+        with conn:
+            cur = conn.execute('''INSERT INTO MC_QUESTION (PID, QUESTION,
+            CATEGORY, DIFFICULTY, CORRECT, WRONG1, WRONG2,
+            WRONG3) VALUES (?,?,?,?,
+            ?,?,?)'''
+
+            1=self.outgoingQuestion.qID,
+            1=self.outgoingQuestion.category,
+            2=self.outgoingQuestion.difficulty,
+            3=self.outgoingQuestion.question,
+            4=self.outgoingQuestion.correct,
+            5=self.outgoingQuestion.wrong1,
+            6=self.outgoingQuestion.wrong2,
+            7=self.outgoingQuestion.wrong3
+
+        conn.commit()
