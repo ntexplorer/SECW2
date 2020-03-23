2a6b843ae4b8167bd854ab84d99843544f44a8a8
ntexplorer <ntexplorer@qq.com>
2020-03-23 14:14:02 +0000

    Deleted unnecessary files.

--- c_1response.py
+++
@@ -1,7 +0,0 @@
-
-class submitResponse:
-    def __init__(self, qID=0, radioValType=0, radioValOption=0):
-
-        self.qID = qID
-        self.radioValType = radioValType
-        self.radioValOption = radioValOption
