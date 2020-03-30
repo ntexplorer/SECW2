db6ac594ab3f3a1b27f44116a649c505d6a16208
clarkey02 <ClarkR8@cardiff.ac.uk>
2020-03-30 11:11:07 +0100

    Cleared Folder

--- Unit2/u2_response_class.py
+++
@@ -1,9 +0,0 @@
-
-# Shelved Response Class
-
-class submitResponse:
-    def __init__(self, qID=0, radioValType=0, radioValOption=0):
-
-        self.qID = qID
-        self.radioValType = radioValType
-        self.radioValOption = radioValOption
